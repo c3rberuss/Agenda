ea654641254770026c4e226105ac47e40620c085
c3rberuss <aaamaya98@gmail.com>
2017-11-08 22:28:02 -0600

    nothink

--- Agenda/src/agenda/Agenda.java
+++ Agenda/src/agenda/Agenda.java
@@ -15,7 +15,10 @@ public class Agenda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        System.out.println("Hora de trabajar papu >:v");
+        
     }
     
 }
